a5e6009f085e19733db1761c994997e4a0e52fa4
Wolroys <alekseypaslenov@mail.ru>
2023-01-02 17:17:21 +0300

    complete

---
+++ src/WeightedGraphs/Deyk/WDGraph.java
@@ -0,0 +1,15 @@
+package WeightedGraphs.Deyk;
+
+
+class DistPar{
+    public int distance;
+    public int parentVert;
+
+    public DistPar(int distance, int parentVert){
+        this.distance = distance;
+        this.parentVert = parentVert;
+    }
+}
+public class WDGraph {
+
+}
